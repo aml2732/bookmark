263a0042f51776e4050f27cc64342e8903e01c26
Angela Lloyd <aml2732@utexas.edu>
2017-09-15 08:16:44 -0500

    add basic routing between pages

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import Header from './Header.js';
 import MyBookmarks from './MyBookmarks.js';
 import Authentication from './Authentication.js';
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+class Home extends Component{
+  render(){return(
+    <div>Placeholder for Home</div>
+  );}
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <Header />
-        <Authentication />
-        <MyBookmarks />
-      </div>
+      <Router>
+        <div className="container">
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route path="/authentication" component={Authentication} />
+          <Route path="/dashboard" component={MyBookmarks} />
+        </div>
+      </Router>
     );
   }
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 class Header extends Component {
   render() {
     return(
       <div className="header">
         Header goes here
+        <div className="authentication-navigation">
+          <Link to="/authentication" >Sign in</Link>
+        </div>
+        <div className="classic-navigation">
+          <ul>
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li>Nav-Placeholder</li>
+          </ul>
+        </div>
       </div>
     );
   }
