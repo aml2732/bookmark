d6000559ea58174bf483ae1c5122c18f7f988aa9
Angela Lloyd <aml2732@utexas.edu>
2017-09-22 09:56:29 -0500

    playing around

--- src/App.js
+++ src/App.js
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import './styles/App.css';
 import Header from './Header.js';
 import MyBookmarks from './MyBookmarks.js';
-import Authentication from './Authentication.js';
+import {Authentication, authLogic} from './Authentication.js';
 
 import {
   BrowserRouter as Router,
   Route,
-  Link
+  Link,
+  Redirect,
+  withRouter
 } from 'react-router-dom'
+
 
 class Home extends Component{
   render(){return(
     <div>Placeholder for Home</div>
   );}
 }
+
+function PrivateRouteComponent({component: Component, ...rest}) {
+  if (authLogic.isAuthenticated){
+    console.log("is Authenticated");
+    return <Route {...rest} render={(props) => (<Component {...props} />)} />
+  }
+  else{
+    console.log("is not authenticated");
+    return (<Route {...rest} render={(props) => {
+      <Redirect to={{
+        pathname: '/authentication',
+        state: {from: props.location}
+      }} />
+    }}/>);
+  }
+}
+
 
 class App extends Component {
   render() {
@@ -24,7 +44,7 @@ class App extends Component {
           <Header />
           <Route exact path="/" component={Home} />
           <Route path="/authentication" component={Authentication} />
-          <Route path="/dashboard" component={MyBookmarks} />
+          <PrivateRouteComponent path="/dashboard" component={MyBookmarks}/>
         </div>
       </Router>
     );
--- src/Authentication.js
+++ src/Authentication.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
 import './styles/Authentication.css';
 
+
+const authLogic = {
+  isAuthenticated: false,
+  authenticate(callback){
+    this.isAuthenticated = true;
+    callback();
+  },
+  signout(callback){
+    this.isAuthenticated = false;
+    callback();
+  }
+};
+
 class Authentication extends Component {
+  state = {
+    authenticated: false,
+    userid: "",
+    username: "",
+  }
+
+  login() {
+    authLogic.authenticate(() =>{
+      this.setState({authenticated: authLogic.isAuthenticated});
+    });
+  }
+
+  logout() {
+    authLogic.signout(() => {
+      this.setState({authenticated: authLogic.isAuthenticated});
+    });
+  }
+
   render() {
     return (
       <div className="authentication">
@@ -9,18 +40,19 @@ class Authentication extends Component {
           <h3>Login:</h3>
           <input type="text" placeholder="email"/>
           <input type="text" placeholder="password" />
-          <button>Submit</button>
+          <button onClick={this.login}>Login</button>
         </div>
         <div className="authentication-box">
           <h3>Sign Up:</h3>
           <input type="text" placeholder="email"/>
           <input type="text" placeholder="password" />
-          <input type="text" placeholder="password" /> 
-          <button>Submit</button>
+          <input type="text" placeholder="password" />
+          <button>Signup</button>
         </div>
       </div>
     );
   }
 }
 
-export default Authentication;
+//export default Authentication;
+export {Authentication, authLogic};
